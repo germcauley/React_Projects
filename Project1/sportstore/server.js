9e7b43324a255f8c44866bba765809e2ac9fd5c8
germcauley <geraldmcauley@gmail.com>
2019-08-10 23:10:22 +0100

    app runs but cannot import fontawesome or botstrap yet

--- Project1/sportstore/server.js
+++ Project1/sportstore/server.js
@@ -3,7 +3,8 @@ const jsonServer = require("json-server");
 const chokidar = require("chokidar");
 const cors = require("cors");
 
-const fileName = process.argv[2] || "./data.js" const port = process.argv[3] || 3500;
+const fileName = process.argv[2] || "./data.js" 
+const port = process.argv[3] || 3500;
 let router = undefined;
 const app = express();
 const createServer = () => {    
